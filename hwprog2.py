b8b23c0310f09f21074ec5a2a65eb32a7030009f
dashajarikova <d3306642@mail.ru>
2016-11-07 17:30:15 +0400

    Add files via upload

---
+++ hwprog2.py
@@ -0,0 +1,12 @@
+print ("Введите число")
+num=int(input())
+print ("Введите слово")
+w=input()
+while w!= "программирование" and w!= "программирование":
+    for i in range(num):
+        print (w)
+    print ("Введите число")
+    num=int(input())
+    print ("Введите слово")
+    w=input()
+print ("Конец")
