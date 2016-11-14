b868a1cbba5252d029f3d74ff36a7593b979cbc4
dashajarikova <d3306642@mail.ru>
2016-11-15 00:28:14 +0400

    Update hwprog3.py

--- hwprog3.py
+++ hwprog3.py
@@ -1,8 +1,8 @@
-arr = []
+w = []
 while True:
     word =(input('Введите латинское слово: '))
     if not word: break
     elif word[-2:]== 're' or word [-2:]=='ri':
-        arr.append(word)
-for i in range (len(arr)):
-    print (arr[i])
+        w.append(word)
+for i in range (len(w)):
+    print (w[i])
