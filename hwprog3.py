6aa8a054cff38da2f1340ca01a63dd6ca0537397
dashajarikova <d3306642@mail.ru>
2016-11-15 00:00:00 +0400

    Delete hwprog3.py

--- hwprog3.py
+++
@@ -1,8 +0,0 @@
-arr = []
-while True:
-    word =(input('Введите латинское слово: '))
-    if not word: break
-    elif word[-1]== 'e' and word [-2]=='r':
-        arr.append(word)
-for i in range (len(arr)):
-    print (arr[i])
