ae80f499a6b4b91cd47d571e1edad0b0a7ca6450
dashajarikova <d3306642@mail.ru>
2017-02-13 19:25:57 +0400

    Add files via upload

---
+++ hwlast/hw13.py
@@ -0,0 +1,12 @@
+import re
+def change():
+    with open ('text.txt', 'r', encoding='utf-8') as source:
+        old_text=source.readlines()
+    with open ('endtext.txt', 'w', encoding='utf-8') as blocknote:
+        for line in old_text:
+            newline=re.sub(r'(\s|\(|«)?философи((я|и|ю|е)(м|й|ю|х)?и?(\s|.|,|:|;)?(\[|\s|\)|»))', r'\1астрологи\2', line, flags=re.DOTALL)
+            newnewline=re.sub(r'(\s|\(|«)?Философия((а|о|у|е|ы)?(м|в|х)?и?(\s|.|,|:|;)?(\[|\s|»))', r'\1Астрологи\2', newline, flags=re.DOTALL)
+            blocknote.write(newnewline)
+    return 'Сделано!'
+
+change()
