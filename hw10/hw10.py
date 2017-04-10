45b592788cfec62a5e73d240df751613d7e174b1
dashajarikova <d3306642@mail.ru>
2017-04-11 00:56:54 +0400

    Add files via upload

--- hw10/hw10.py
+++ hw10/hw10.py
@@ -1,8 +1,9 @@
 def dictr():
     text=input('Введите текст: ')
     sen_spl=text.split ('.')
-    words=text.split()
-    dict1={i: len(i) for i in words}
-    dict2={a : dict1 for a in sen_spl}
+    for sentence in sen_spl:
+        words=sentence.split()
+        dict1={i: len(i) for i in words}
+        dict2={sentence: dict1 for sentence in sen_spl}
     print (dict2)
 dictr ()
