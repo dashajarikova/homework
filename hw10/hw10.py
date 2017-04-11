5290ae3076a04b387bea8b407cd482666f18a65f
dashajarikova <d3306642@mail.ru>
2017-04-11 22:54:30 +0400

    Update hw10.py

--- hw10/hw10.py
+++ hw10/hw10.py
@@ -1,9 +1,10 @@
 def dictr():
     text=input('Введите текст: ')
     sen_spl=text.split ('.')
+    dict2={}
     for sentence in sen_spl:
         words=sentence.split()
         dict1={i: len(i) for i in words}
-        dict2={sentence: dict1 for sentence in sen_spl}
+        dict2[sentence]=dict1
     print (dict2)
 dictr ()
